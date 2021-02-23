2e75888e4df77658377061fc4d47833674caf2d2
lakshmilanka99 <lakshmi.lanka@aa.com>
2021-02-22 23:05:15 -0600

    app changes

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 
-In this article, I am going to explain how to create, publish Azure functions and integrate with SQL data base in java.
+In this article, I am going to explain how to create, publish Azure functions using java.
 
 You Will Learn
 How can we create and publish azure functions by using java 
 1.HttpTrigger Function
-2.QueueTrigger
-3.BlobTrigger
-4.TimeTrigger
-5. HttpTrigger Function integrate with Azure SQL
 
 Tools Required
 •	Maven 3.0+ build tool
--- src/main/java/com/kish/AzureFunctions/HttpTriggerFunction.java
+++ src/main/java/com/kish/AzureFunctions/HttpTriggerFunction.java
@@ -28,49 +28,22 @@ public class HttpTriggerFunction {
      */
     @FunctionName("HttpExample")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) 
-            HttpRequestMessage<Optional<Employee>> request,
-            final ExecutionContext context,
-            @QueueOutput(name = "httpQueue", queueName = "httpRequestQueue", connection = "AzureWebJobsStorage") final OutputBinding<Employee> result) {
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
-        String name = request.getQueryParameters().get("name");
-         Employee emp =null;
-        if(request.getBody()!=null)
-        {
-         emp = request.getBody().get();
-        context.getLogger().info("emp id."+emp.getId());
-        context.getLogger().info("emp Name."+emp.getEname());
-        try
-        {
-        	    // Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        	      String url ="";
-                 Connection connect = DriverManager.getConnection(url);
-                 String sql="INSERT INTO dbo.data_trx(id,name,email,ssn) Values (?,?,?,?)";
-                 PreparedStatement statement = connect.prepareStatement(sql);
-                 statement.setInt(1, emp.getId());
-                 statement.setString(2, emp.getEname());
-                 statement.setString(3, emp.getEmail());
-                 statement.setString(4, emp.getSsn());
-                 statement.execute();
-                 }
-                 catch(Exception e)
-                 {
-                 	return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Exception "+e.getMessage()).build();
-                 }
-        
-        
-        }
-        result.setValue(emp);
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Good Day , "+emp.getId() +" "+ name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
     }
-
-  
-    
-
 }
